51404e15d91bb7727033abb7c29518125e015760
NhutCorp <info@nhutcorp.com>
2017-09-16 12:59:11 +0900

    Add Material configuration to SystemJS

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
 import { SeedConfig } from './seed.config';
-// import { ExtendPackages } from './seed.config.interfaces';
+import { ExtendPackages } from './seed.config.interfaces';
 
 /**
  * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
@@ -22,6 +22,8 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
+      { src: 'hammerjs/hammer.min.js', inject: 'libs' },
+      { src: 'web-animations-js/web-animations.min.js', inject: 'libs' },
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
     ];
@@ -42,14 +44,67 @@ export class ProjectConfig extends SeedConfig {
       //{'node_modules/immutable/dist/immutable.js': [ 'Map' ]},
     ];
 
-    // Add packages (e.g. ng2-translate)
-    // let additionalPackages: ExtendPackages[] = [{
-    //   name: 'ng2-translate',
-    //   // Path to the package's bundle
-    //   path: 'node_modules/ng2-translate/bundles/ng2-translate.umd.js'
-    // }];
-    //
-    // this.addPackagesBundles(additionalPackages);
+    let additionalPackages: ExtendPackages[] = [
+      {
+        name: '@angular/material',
+        path: 'node_modules/@angular/material/bundles/material.umd.js',
+        packageMeta: {
+          defaultExtension: 'js'
+        }
+      },
+      {
+        name: '@angular/cdk',
+        path: 'node_modules/@angular/cdk/bundles/cdk.umd.js'
+      },
+      {
+        name: '@angular/cdk/a11y',
+        path: 'node_modules/@angular/cdk/bundles/cdk-a11y.umd.js'
+      },
+      {
+        name: '@angular/cdk/bidi',
+        path: 'node_modules/@angular/cdk/bundles/cdk-bidi.umd.js'
+      },
+      {
+        name: '@angular/cdk/coercion',
+        path: 'node_modules/@angular/cdk/bundles/cdk-coercion.umd.js'
+      },
+      {
+        name: '@angular/cdk/collections',
+        path: 'node_modules/@angular/cdk/bundles/cdk-collections.umd.js'
+      },
+      {
+        name: '@angular/cdk/keycodes',
+        path: 'node_modules/@angular/cdk/bundles/cdk-keycodes.umd.js'
+      },
+      {
+        name: '@angular/cdk/observers',
+        path: 'node_modules/@angular/cdk/bundles/cdk-observers.umd.js'
+      },
+      {
+        name: '@angular/cdk/overlay',
+        path: 'node_modules/@angular/cdk/bundles/cdk-overlay.umd.js'
+      },
+      {
+        name: '@angular/cdk/platform',
+        path: 'node_modules/@angular/cdk/bundles/cdk-platform.umd.js'
+      },
+      {
+        name: '@angular/cdk/portal',
+        path: 'node_modules/@angular/cdk/bundles/cdk-portal.umd.js'
+      },
+      {
+        name: '@angular/cdk/rxjs',
+        path: 'node_modules/@angular/cdk/bundles/cdk-rxjs.umd.js'
+      },
+      {
+        name: '@angular/cdk/scrolling',
+        path: 'node_modules/@angular/cdk/bundles/cdk-scrolling.umd.js'
+      },
+      {
+        name: '@angular/cdk/table',
+        path: 'node_modules/@angular/cdk/bundles/cdk-table.umd.js'
+      }
+    ];
 
     /* Add proxy middleware */
     // this.PROXY_MIDDLEWARE = [
